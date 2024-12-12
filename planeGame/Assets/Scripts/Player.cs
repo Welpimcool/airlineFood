b6f76f7ea7d6c19c7cac007d45fafba53f37b1d1
Partybox98 <kirbymmo@gmail.com>
2024-12-12 15:27:56 -0800

    added interactions
    adds a function in the player code for interacting with objects, it detects with raycasts but currently always returns null due to the compare tag statement

--- planeGame/Assets/Scripts/Player.cs
+++ planeGame/Assets/Scripts/Player.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector2 direction;
+    private Vector2 inpDirection;
     private Rigidbody2D body;
+    private GameObject objHolding;
+    public string interactionTag = "Interactable";
     public int walkSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        inpDirection = Vector2.up;
     }
 
     // Update is called once per frame
@@ -20,12 +24,31 @@ public class Player : MonoBehaviour
         direction.y = Input.GetAxisRaw("Vertical");
         direction.Normalize();
 
+        if (direction != Vector2.zero) {
+            inpDirection = direction;
+        }
+
         body.velocity = direction.normalized * walkSpeed;
         
+        if (Input.GetKey(KeyCode.E)) {
+            pickup();
+        }
     }
 
     void FixedUpdate() 
     {
         body.velocity = direction * walkSpeed;
+    }
+    void pickup() {
+
+        RaycastHit2D hit = Physics2D.Raycast(body.position,inpDirection,10f);
+        Debug.DrawRay(body.position,inpDirection*10f,Color.yellow);
+        if (hit) {
+            if (hit.collider.CompareTag(interactionTag)) {
+                Debug.Log("Hit something: "+hit.collider.name);
+            }
+            
+        }
+
     }
 }
