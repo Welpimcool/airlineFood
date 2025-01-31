613577157f4405ed75385f16b4ab8b6ac39c7cc4
Welpimcool <bo1bo2bo3bo1bo1bo1@gmail.com>
2025-01-31 15:25:59 -0800

    Very Basic Main Menu

--- planeGame/Assets/Scripts/Meat.cs
+++ planeGame/Assets/Scripts/Meat.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Meat : Ingredient
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+//public class Meat : Ingredient
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!prop)
-        {
-            GetComponentInChildren<Meter>().setValue(value);
-            if (value >= maxValue)
-            {
-                value = 0;
-                state += 1;
-                Debug.Log("state increased:" + state);
-            }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (!prop)
+//        {
+//            GetComponentInChildren<Meter>().setValue(value);
+//            if (value >= maxValue)
+//            {
+//                value = 0;
+//                state += 1;
+//                Debug.Log("state increased:" + state);
+//            }
 
-        }
-    }
-}
+//        }
+//    }
+//}
