60e16d02bb820c5430c62f9715c153b4b0da8093
Welpimcool <bo1bo2bo3bo1bo1bo1@gmail.com>
2025-02-21 15:26:44 -0800

    Fixed Passenger and order control

--- planeGame/Assets/Scripts/Passenger.cs
+++ planeGame/Assets/Scripts/Passenger.cs
@@ -1,43 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Passenger : MonoBehaviour
 {
     private float anger;
-    private float orderTime;
-    private bool isOrderActive;
+    private bool isOrderActive = false;
     private float timeRemaining;
 
+   
+
     void Start()
-    {
-        orderTime = 60;
-        StartCoroutine(Order(orderTime));
+    { 
         GetComponentInChildren<Canvas>().enabled = false;
     }
+    //Gets time remaining in order
     public float getTimeRemaining()
     {
         return timeRemaining;
     }
-    public void test()
+    //Gets whether the passenger has already ordered
+    public bool getIsOrderActive()
     {
-        Order(orderTime);
+        return isOrderActive;
     }
+    //Processes the actual order
     public IEnumerator Order(float orderTime)
     {
+        isOrderActive = true;
+        yield return new WaitForSeconds(1);
+        //Prepares Meter
+        GetComponentInChildren<Meter>().setMaxValue(orderTime);
         GetComponentInChildren<Canvas>().enabled = true;
+
         float timeRemaining = orderTime;
+        //Makes time tick down
         while (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
             GetComponentInChildren<Meter>().setValue(timeRemaining);
+            //Makes anger increase if order took too long
             if (timeRemaining < orderTime / 4)
             {
                 anger += Time.deltaTime;
             }
             yield return null;
-            //Debug.Log(timeRemaining);
-            //Debug.Log(anger);
             if (anger >= 5)
             {
                 GetComponentInParent<SpriteRenderer>().color = Color.red;
--- planeGame/Assets/Scripts/PassengerManager.cs
+++ planeGame/Assets/Scripts/PassengerManager.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class PassengerManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] PassengerList; 
+    [SerializeField] GameObject[] PassengerList;
+    GameObject selectedPassenger;
     // Start is called before the first frame update
     void Start()
+    {  
+        for(int i = 0; i < 20; i++)
+        {
+            SelectPassenger();
+        }
+    }
+    //Picks a random passenger to place an order, only picks one that isnt already chosen
+    public void SelectPassenger()
     {
-        GameObject testing = PassengerList[Random.Range(0, 20)];
-        testing.GetComponentInChildren<Passenger>().test();
-        Debug.Log(testing);
+        selectedPassenger = PassengerList[Random.Range(0, PassengerList.Length)];
+        if (selectedPassenger.GetComponent<Passenger>().getIsOrderActive() == false)
+        {
+            selectedPassenger.GetComponent<Passenger>().StartCoroutine(selectedPassenger.GetComponent<Passenger>().Order(60));
+            Debug.Log(selectedPassenger);
+        }
+        else
+        {
+            Debug.Log("reroll");
+            SelectPassenger();
+        }
+    }
+    public void OrderComplete()
+    {
 
     }
-
     // Update is called once per frame
     void Update()
     {
