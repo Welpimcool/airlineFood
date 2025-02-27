7343c758b840363c4bced7ca50ae54c8680294c4
Welpimcool <bo1bo2bo3bo1bo1bo1@gmail.com>
2025-02-27 15:27:14 -0800

    More weether

--- planeGame/Assets/Scripts/Passenger.cs
+++ planeGame/Assets/Scripts/Passenger.cs
@@ -15,44 +15,42 @@ public class Passenger : MonoBehaviour
     { 
         GetComponentInChildren<Canvas>().enabled = false;
     }
-    //Gets time remaining in order
+//Gets time remaining in order
     public float getTimeRemaining()
     {
         return timeRemaining;
     }
-    //Gets whether the passenger has already ordered
+ //Gets whether the passenger has already ordered
     public bool getIsOrderActive()
     {
         return isOrderActive;
     }
-    //Processes the actual order
+//Processes the actual order
     public IEnumerator Order(float orderTime)
     {
         bool sensitive = Random.Range(0, 2) == 1;
         float timeRemaining = orderTime;
-        //Makes time tick down
+ //Makes time tick down
         if (angy)
         {
             timeRemaining -= orderTime / 5;
         }
-
         isOrderActive = true;
         yield return new WaitForSeconds(1);
-        //Prepares Meter
+//Prepares Meter
         GetComponentInChildren<Meter>().setMaxValue(orderTime);
         GetComponentInChildren<Canvas>().enabled = true;
-
         while (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
             GetComponentInChildren<Meter>().setValue(timeRemaining);
-            //Makes anger increase if order took too long
+//Makes anger increase if order took too long
             if (timeRemaining < orderTime / 10 || (sensitive && timeRemaining < orderTime/5 && WeatherShift.weather ==1))
             {
                 angy = true;
                 GetComponentInParent<SpriteRenderer>().color = Color.red;
             }
-            //idk why this is needed but it is
+//idk why this is needed but it is
             yield return null;
         }
         GetComponentInChildren<Canvas>().enabled = false;
--- planeGame/Assets/Scripts/WeatherShift.cs
+++ planeGame/Assets/Scripts/WeatherShift.cs
@@ -6,21 +6,47 @@ public class WeatherShift : MonoBehaviour
 {
     [SerializeField]public GameObject sky;
     [SerializeField] public GameObject rain;
+    [SerializeField] public GameObject snow;
+    [SerializeField] public GameObject powerOut;
     public static int weather = 0;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            
+            powerOut.SetActive(false);
+            weather = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+           
+            powerOut.SetActive(false);
+            weather = 0;
+            snow.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            powerOut.SetActive(true);
+            weather = 2;
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            snow.SetActive(true);
+            weather = 3;
+
+        }
+        if (weather == 1)
         {
             sky.SetActive(true);
             rain.SetActive(true);
-            weather = 1;
         }
-        if (Input.GetKeyDown(KeyCode.R))
+        else
         {
             sky.SetActive(false);
             rain.SetActive(false);
-            weather = 0;
         }
     }
 }
