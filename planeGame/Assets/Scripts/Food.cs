9a8587fa4c0e86641159da73a8ada468fc0d00df
Partybox98 <kirbymmo@gmail.com>
2025-03-04 14:12:16 -0800

    Before adding ingredients

--- planeGame/Assets/Scripts/Food.cs
+++ planeGame/Assets/Scripts/Food.cs
@@ -11,6 +11,7 @@ public class Food : Ingredient
     // Start is called before the first frame update
     void Start()
     {
+        transform.localScale = new Vector3(scale,scale,0);
         if (!prop) {
         // this.setScale(1);
         this.setState(0);
