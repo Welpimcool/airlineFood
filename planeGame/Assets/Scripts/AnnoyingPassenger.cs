666091f2c9cf8ff014da44585696e86865322744
Welpimcool <bo1bo2bo3bo1bo1bo1@gmail.com>
2025-03-07 15:25:59 -0800

    pain

--- planeGame/Assets/Scripts/AnnoyingPassenger.cs
+++ planeGame/Assets/Scripts/AnnoyingPassenger.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AnnoyingPassenger : MonoBehaviour
 {
-    public int side = 0;
+    public bool left;
+    public Vector3 passengerPosition;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        passengerPosition = GetComponentInParent<Transform>().position;
+        left = (passengerPosition.x <= -5);
+        imWalkinEre();
     }
 
     // Update is called once per frame
@@ -18,10 +26,16 @@ public class AnnoyingPassenger : MonoBehaviour
     }
     IEnumerator imWalkinEre() 
     {
-        if(side == 0)
-        {
-            
-        }
+        //if(left)
+        //{
+        //    passengerPosition.x += 4;
+        //}
+        //else
+        //{
+        //    passengerPosition.x -= 4;
+        //}
+        agent.SetDestination(target.position);
+
         yield return null;
     }
 }
