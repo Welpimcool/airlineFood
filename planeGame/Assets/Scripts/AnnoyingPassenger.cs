b7e8add28223ffc1abb3b79a928e576cd9b799b0
Welpimcool <bo1bo2bo3bo1bo1bo1@gmail.com>
2025-03-11 15:29:49 -0700

    Annoying passenger!!!

--- planeGame/Assets/Scripts/AnnoyingPassenger.cs
+++ planeGame/Assets/Scripts/AnnoyingPassenger.cs
@@ -1,41 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AnnoyingPassenger : MonoBehaviour
 {
+    public Vector3 origin;
     public bool left;
     public Vector3 passengerPosition;
-    [SerializeField] NavMeshAgent agent;
+    NavMeshAgent agent;
+    [SerializeField] Transform transform;
     [SerializeField] Transform target;
+    [SerializeField] Transform bathroom;
+    int i = 0;
     // Start is called before the first frame update
     void Start()
     {
+        
+        origin = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+        agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
         passengerPosition = GetComponentInParent<Transform>().position;
         left = (passengerPosition.x <= -5);
-        imWalkinEre();
+        StartCoroutine(imWalkinEre());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (passengerPosition.y == target.position.y)
+        //{
+        //    Debug.Log("Here");
+        //}
+        //if (agent.remainingDistance < 0.1)
+        //{
+        //    if (i == 0)
+        //    {
+        //        agent.SetDestination(bathroom.position);
+        //        StartCoroutine(bathroomBreak());
+        //        i = 1;
+        //    }
+        //}
     }
+
     IEnumerator imWalkinEre() 
     {
-        //if(left)
-        //{
-        //    passengerPosition.x += 4;
-        //}
-        //else
-        //{
-        //    passengerPosition.x -= 4;
-        //}
+        if (left)
+        {
+            passengerPosition.x += 4;
+        }
+        else
+        {
+            passengerPosition.x -= 4;
+        }
         agent.SetDestination(target.position);
+        while (true)
+        {
+            if (passengerPosition.y == target.position.y)
+            {
+                Debug.Log("Here");
+            }
+            if (agent.remainingDistance < 0.1)
+            {
+                if (i == 0)
+                {
+                    agent.SetDestination(bathroom.position);
+                    i = 1;
+                    yield return new WaitForSeconds(10);
+                    Debug.Log("done");
+                    agent.enabled = false;
+                    transform.position = origin;
 
+                
+                    break;
+                }
+                
+            }
+            yield return new WaitForSeconds(0.1f);
+         
+        }
         yield return null;
     }
+    //IEnumerator bathroomBreak()
+    //{
+    //    Debug.Log("starting");
+        
+        
+    //}
+    
 }
