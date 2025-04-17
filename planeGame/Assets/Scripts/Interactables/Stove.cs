aad02c6aa11e6dd8f89694f6f920ba556c25b960
Partybox98 <kirbymmo@gmail.com>
2025-04-17 10:28:01 -0700

    Ready for 0.2.0 (annoying pass doesnt work)

--- planeGame/Assets/Scripts/Interactables/Stove.cs
+++ planeGame/Assets/Scripts/Interactables/Stove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Stove : Table
 {
-    public static float speed = 1;
+    public static float speed = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
