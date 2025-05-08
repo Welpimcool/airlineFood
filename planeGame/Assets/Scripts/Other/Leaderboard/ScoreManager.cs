acce7c65cc2d2402d88b1a0a7f14e612053d0a66
Welpimcool <bo1bo2bo3bo1bo1bo1@gmail.com>
2025-05-07 17:56:04 -0700

    Global leaderboard

---
+++ planeGame/Assets/Scripts/Other/Leaderboard/ScoreManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputName;
+
+    public UnityEvent<string, int> submitScoreEvent;
+    public void submitScore()
+    {
+        submitScoreEvent.Invoke(inputName.text, PassengerManager.ordersCompleted);
+    }
+}
